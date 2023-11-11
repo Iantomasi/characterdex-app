4df065744cb58bb9743644f623a494a80bcc532b
Giuliano <giulianoiantomasi@gmail.com>
2023-11-11 13:38:44 -0500

    Add files via upload

---
+++ CharacterDex/CharacterDex/CharacterDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterDexApp.swift
+//  CharacterDex
+//
+//  Created by ella iantomasi on 2023-11-11.
+//
+
+import SwiftUI
+
+@main
+struct CharacterDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CharacterDex/CharacterDex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CharacterDex
+//
+//  Created by ella iantomasi on 2023-11-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
